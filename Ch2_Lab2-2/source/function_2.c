d0992e765f02d23c1c03377b7e4b70f14dd57105
liu <jason030731@gmail.com>
2017-10-03 15:34:17 +0800

    10/3 update

---
+++ Ch2_Lab2-2/source/function_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int Han_Xin_2(void)
+{
+	char sign = 'X';
+	char sign2 = '-';
+	int i, j;
+	printf("(2)\n");
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j<10; j++)
+			if ((i == 0) || (i == 9) || (i == 1) || (i == 2) || (i == 8) || (i == 7) || (j == 0) || (j == 8) || (j == 7) || (j == 2) || (j == 1) || (j == 9))
+			{
+				printf("%c", sign);
+			}
+			else
+			{
+				printf("%c", sign2);
+			}
+		printf("\n");
+	}
+}
